59262fa4dac78d10338bb994da29ec8e9f86a78d
ayazalii <ayazali935915@gmail.com>
2026-05-09 10:43:52 +0530

    Normal

---
+++ Day 9/array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int arr[5] ; //{1,2,3,4,5};
+
+     //MANUAL ARRAY OF INPUTS USING cin 
+//      cin>>arr[0];
+// cin>>arr[1];
+// cin>>arr[2];
+// cin>>arr[3];
+// cin>>arr[4];
+
+int size= sizeof(arr)/sizeof(arr[0]);
+
+
+cout<<"Enter elements one by one: "<<endl;
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
